a645468b16105ede262d7506a91bf23d9ae6848c
Leila Brum <leilabrum@gmail.com>
2023-03-29 08:56:48 -0300

    Base da livraria

---
+++ Livraria/Atendente_Livraria.java
@@ -0,0 +1,33 @@
+package Saraiva;
+
+import javax.swing.JOptionPane;
+public class Atendente_Livraria {
+	public static void main (String args[]) {
+		Livraria cultura = new Livraria();
+		
+		String a, b, c, d, e, i;
+		int f, g;
+		double h;
+		
+		JOptionPane.showMessageDialog(null, "Categoria: digite 1 para Romance \n 2 para Espírita \n 3 para Informática");
+		a = JOptionPane.showInputDialog (null, "Código");
+		b = JOptionPane.showInputDialog (null, "Editora");
+		c = JOptionPane.showInputDialog (null, "Autor");
+		d = JOptionPane.showInputDialog (null, "Livro");
+		e = JOptionPane.showInputDialog (null, "Quantidade");
+		i = JOptionPane.showInputDialog (null, "Preço");
+		
+		f = Integer.parseInt(a);
+		g = Integer.parseInt(e);
+		h = Double.parseDouble(i);
+		
+		cultura.editora = b;
+		cultura.autor = c;
+		cultura.livro = c;
+		cultura.quantidade = g;
+		cultura.preco = h;
+		
+		cultura.Informa_Livros(f);
+				
+	}
+}
---
+++ Livraria/Livraria.java
@@ -0,0 +1,29 @@
+package Saraiva;
+
+import javax.swing.JOptionPane;
+
+public class Livraria {
+	String editora;
+	String autor;
+	String livro;
+	int quantidade;
+	double preco;
+	
+	public void Informa_Livros (int codigo) {
+		switch(codigo) {
+		case 1: JOptionPane.showMessageDialog(null, "CATEGORIA ROMANCE \n" +
+		"LIVRO: " + livro + "\n EDITORA: " + editora + "\n AUTOR: " + autor +
+		"\n QUANTIDADE: " + quantidade + "\n PREÇO: R$" + preco, " ", JOptionPane.PLAIN_MESSAGE); break;
+		
+	case 2: JOptionPane.showMessageDialog(null, "CATEGORIA ESPÍRITA \n" +
+	"LIVRO: " + livro + "\n EDITORA: " + editora + "\n AUTOR: " + autor +
+	"\n QUANTIDADE: " + quantidade + "\n PREÇO: R$" + preco, " ", JOptionPane.PLAIN_MESSAGE); break;
+	
+	case 3: JOptionPane.showMessageDialog(null, "CATEGORIA INFORMÁTICA \n" +
+			"LIVRO: " + livro + "\n EDITORA: " + editora + "\n AUTOR: " + autor +
+			"\n QUANTIDADE: " + quantidade + "\n PREÇO: R$" + preco, " ", JOptionPane.PLAIN_MESSAGE); break;
+	
+	default: System.out.println("Código não corresponde a nenhuma categoria de livro");
+		}
+	}
+}
