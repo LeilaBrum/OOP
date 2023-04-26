523cde35b5fe3a63431bbdd148b8d3d8ef3f3d0a
Leila Brum <leilabrum@gmail.com>
2023-04-26 08:17:05 -0300

    criando receita

---
+++ Herança/Receita/Contribuinte.java
@@ -0,0 +1,10 @@
+package Herança.Receita;
+public class Contribuinte{
+    private String nome;
+    public void setNome(String Nome){
+        this.nome = nome;
+    }
+    public getNome(){
+        return(nome);
+    }
+}
