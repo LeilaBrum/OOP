b7efce8abad8427f08d73bab50eb8eeba0295932
Leila Brum <leilabrum@gmail.com>
2023-04-26 08:20:15 -0300

    criando pessoa fisica

---
+++ Herança/Receita/PessoaFisica.java
@@ -0,0 +1,11 @@
+package Herança.Receita;
+
+public class PessoaFisica extends Contribuinte{
+    private String rg;
+    public void setRG(String rg){
+        this.rg=rg;
+    }
+    public String getRG(){
+        return (rg);
+    }
+}
