551149eaa078ea2134253612af0aac0215ad3b16
Marina <maricamano@hotmail.com>
2023-05-17 08:38:58 -0300

    Aprendendo sobrecarga

---
+++ Sobrecarga_Métodos/funciona_sobrecarga.java
@@ -0,0 +1,14 @@
+package metodos;
+import javax.swing.JOptionPane;
+public class funciona_sobrecarga {
+	public static void main (String args[]) {
+		Ficha_funcionarios funcionarios = new Ficha_funcionarios (22, 2880.09, "Gisele", "Pedagoga");
+		JOptionPane.showMessageDialog(null, funcionarios.mostra_funcionarios());
+		
+		concatena_aplicacao();
+	}
+	
+	public static void concatena_aplicacao () {
+		Acesso_secundario a = new Acesso_secundario();
+	}
+}
